f6be54ab99ba383b9e6840ac6e278a6facc33d2f
Isaac Torres <isaac@isaactorz.dev>
2023-04-01 16:10:11 -0600

    add some c tests

---
+++ c11.c
@@ -0,0 +1,6 @@
+#include <stdatomic.h>
+
+int main() {
+    _Atomic(int) a;
+    return 0;
+}
---
+++ c17.c
@@ -0,0 +1,7 @@
+int main() {
+#if __STDC_VERSION__ == 201710L
+    return 0;
+#else
+    return 1;
+#endif
+}
